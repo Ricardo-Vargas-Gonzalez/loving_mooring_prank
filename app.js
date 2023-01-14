339284885d4260935e1d3203a42696d98e3105f4
Ricardo-Vargas-Gonzalez <ceinnova05251@innovaccion.mx>
2023-01-14 09:40:33 -0600

    updating

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function moverPosicionRandom(elm) {
+function randomPositionMovement(elm) {
     elm.style.position = 'absolute'; /*Esto permite que su movimiento pueda llevarse a cabo por toda la pantalla */
     elm.style.top = Math.random() * (window.innerHeight - elm.offsetHeight) + 'px';
     elm.style.left = Math.random() * (window.innerWidth - elm.offsetWidth) + 'px';
@@ -7,4 +7,12 @@ let btnSi = document.getElementById("btn_si");
 let btnNo = document.getElementById("btn_no");
 let divModoSexo = document.getElementsByClassName("modo_zetso")[0];
 
-btnNo.addEventListener('mouseenter', function(e) { moverPosicionRandom(e.target) });
+btnNo.addEventListener('mouseenter', function(e) { randomPositionMovement(e.target) });
+
+btnSi.addEventListener('click', function(e) {
+    alert('Sabía que dirías que sí. Casemonos ya y tengamos hijos, TEEE AAAMOOO!!! <3')
+
+    divModoSexo.style.display = 'block';
+    const song = new Audio('img\\img_modo_hot.mp3')
+    song.play();
+});
