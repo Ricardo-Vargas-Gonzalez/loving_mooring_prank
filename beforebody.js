0fe9fcebc9e224e66b54630b49be0924d5c1ff88
Ricardo-Vargas-Gonzalez <ceinnova05251@innovaccion.mx>
2023-02-11 13:28:43 -0600

    never give up

--- beforebody.js
+++ beforebody.js
@@ -1,3 +1,5 @@
+let encabezados ="";
+
 window.onclick = function() {
     // crea una serie de elements por cada tag
     let encabezados = document.createElement("h1");
@@ -7,3 +9,11 @@ window.onclick = function() {
     document.body.appendChild(encabezados);
 }
 
+// tipos de datos en jaaavascript
+let variable = 42; // numero entero o int
+console.log(typeof variable);
+
+variable = 42; // string o cadena de caracteres, que son letras y de números, por lo que si pones un string se concatenaran, en cambio si lo cambias por un numero se sumarán
+console.log(variable + variable);
+variable = !false; //booleano, solo puede ser sio no, falso o verdadero, no obstante si añades un signo de admiración ! se convierte en un operador, el cual realizará operaciones, dentro con uno dos o hasta tres valores, ya sea suma resta multiplicación o división, en este caso el signo ! es un operador de negación o de complemento, lo que haces es cambiarlo al otro valor 
+console.log(variable);
